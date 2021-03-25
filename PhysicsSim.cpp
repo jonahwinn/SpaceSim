4ba6bee167e118f180ec5a03d8ecf8e8ebcca6a9
Jonah <jonahwinn@live.co.uk>
2021-03-25 17:15:08 +0000

    Basic ata structures and class implementations mostly in place. Needs testing.

--- PhysicsSim.cpp
+++ PhysicsSim.cpp
@@ -10,7 +10,8 @@ Coordinates PhysicsObject::calcG()
 {
 	Coordinates gforceVector;	//temporary vector for current G force calculation
 	Coordinates netForce;		//Net force on the current body.
-	for(PhysicsObject *orbitalBody : localBodies) {
+	for(PhysicsObject *orbitalBody : localBodies) 
+	{
 		gforceVector = position - orbitalBody->position;
 		gforceVector.mag = GCONSTANT * ((mass * orbitalBody->mass) / pow(gforceVector.mag, 2));	//Newton's law of universal gravitation.
 		gforceVector.updateCartesian(); //gets the force vector in cartesian coords based on the above magnitude and existing direction (towards the centre of the orbital body).
@@ -18,19 +19,21 @@ Coordinates PhysicsObject::calcG()
 	}
 	return netForce;
 }
-void PhysicsObject::updateNetForce() {
+void PhysicsObject::updateNetForce() 
+{
 	netForce = calcG() + impulseForce;
 	impulseForce = Coordinates(0, 0);
 }
 //Constructors
 PhysicsObject::PhysicsObject() : mass(5.972e24), position(1.488e9,0), velocity(0,460), accelleration(0,0), netForce(0,0) {} // initialises physics object with default values, corresponding to the Earth's properties.
-PhysicsObject::PhysicsObject(double m, Coordinates p, Coordinates v)
+PhysicsObject::PhysicsObject(std::string n, double m, Coordinates p, Coordinates v)
 {
+	name = n;
 	mass = m;
 	position = p;
 	velocity = v;
 	accelleration = Coordinates(0,0);
-	netForce = Coordinates(0, 0);
+	netForce = Coordinates(0,0);
 }
 //Public Functions
 Coordinates PhysicsObject::calcNextPosition(double tDelta) 
@@ -48,8 +51,33 @@ void PhysicsObject::applyImpulseForce(Coordinates f)
 
 //Solar System implemenation
 SolarSystem::SolarSystem():simulationSpeed(1), tDelta(1), worldBounds(6e9,6e9) {}
-void SolarSystem::nextTick() {
-	for (PhysicsObject currObject : worldObjects) {
+//Public Functions
+void SolarSystem::nextTick() 
+{ //Will need to change later on when doing calculations in multiple threads.
+	for (PhysicsObject currObject : worldObjects) 
+	{
 		currObject.calcNextPosition(tDelta);
+		//could switch this out to have a thread per object.
 	}
+	//Add in draw function here later to draw out a frame. 
+}
+void SolarSystem::addPhysObj(PhysicsObject spaceBody) 
+{
+	worldObjects.push_back(spaceBody);
+}
+void SolarSystem::addPhysObj(std::string name, double mass, Coordinates position, Coordinates velocity) 
+{
+	worldObjects.push_back(PhysicsObject(name, mass, position, velocity));
+}
+void SolarSystem::removeObject(PhysicsObject spaceBody)
+{
+	worldObjects.erase(remove_if(worldObjects.begin(), worldObjects.end(), [spaceBody](const PhysicsObject& obj) {return obj.name == spaceBody.name; }), worldObjects.end());
+}
+void SolarSystem::removeObject(std::string name) 
+{
+	worldObjects.erase(remove_if(worldObjects.begin(), worldObjects.end(), [&name](const PhysicsObject& obj) {return obj.name == name;}), worldObjects.end());
+}
+void SolarSystem::setSpeed(int simSpeed) 
+{
+	simulationSpeed = simSpeed;
 }
--- PhysicsSim.h
+++ PhysicsSim.h
@@ -6,7 +6,7 @@ namespace physicsSim
 {
 	const double GCONSTANT = 6.67430e-11; 
 
-	class PhysicsObject //Class to define a physical object and manipulate its physical properties. 
+	class PhysicsObject //Class to define a physical object within the simulation
 	{
 		double mass;			// in kg
 		Coordinates position;		// in m
@@ -24,7 +24,7 @@ namespace physicsSim
 
 		//Constructors
 		PhysicsObject();
-		PhysicsObject(double m, Coordinates p, Coordinates v); //Initialise physics object with mass m, position p and velocity v.
+		PhysicsObject(std::string name, double m, Coordinates p, Coordinates v); //Initialise physics object with mass m, position p and velocity v.
 		//PhysicsObject(std::string preset); //Allows the user to add a real life planet via name, i.e. PhysicsObject(Earth). #May want to implement this in child classes later. 
 
 		//Setters & Getters
@@ -45,6 +45,7 @@ namespace physicsSim
 		std::vector<PhysicsObject> worldObjects; //array that contains all objects to simulate- could convert to vector later. 
 		Coordinates worldBounds; //bounds of the simulation.
 
+		void calcTimeDelta();
 
 	public:
 		//Constructors
@@ -54,8 +55,9 @@ namespace physicsSim
 		//Class functions
 		void nextTick();
 		void addPhysObj(PhysicsObject spaceBody);
-		void addPhysObj(std::uint64_t m, Coordinates p, Coordinates v);
-		void removeObject(PhysicsObject obj);
+		void addPhysObj(std::string name, double mass, Coordinates position, Coordinates velocity); //add object using parameters
+		void removeObject(PhysicsObject obj);	//remove using object reference
+		void removeObject(std::string name);	//remove using name of object
 		void setSpeed(int simSpeed);
 	};
 }
